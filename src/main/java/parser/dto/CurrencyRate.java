94fd75d712cb864ac3bd9d0cd4ead8028ce56b03
Dmytro Kononenko <rizset290993@gmail.com>
2022-12-03 15:50:09 +0200

    add Getter and Setter

--- src/main/java/parser/dto/CurrencyRate.java
+++ src/main/java/parser/dto/CurrencyRate.java
@@ -7,4 +7,28 @@ public class CurrencyRate {
     BigDecimal sell;
     BigDecimal buy;
     Currencies currency;
+
+    public BigDecimal getSell() {
+        return sell;
+    }
+
+    public void setSell(BigDecimal sell) {
+        this.sell = sell;
+    }
+
+    public BigDecimal getBuy() {
+        return buy;
+    }
+
+    public void setBuy(BigDecimal buy) {
+        this.buy = buy;
+    }
+
+    public Currencies getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currencies currency) {
+        this.currency = currency;
+    }
 }

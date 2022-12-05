4ca45319130f2e1f0f7ab878851e99f2e2ab53f1
madwoman <jkosolap@gmail.com>
2022-12-05 10:08:03 +0200

    NBUParser implemented

--- src/main/java/parser/CurrencyApiService.java
+++ src/main/java/parser/CurrencyApiService.java
@@ -3,9 +3,10 @@ package parser;
 import parser.dto.Currencies;
 import parser.dto.CurrencyRate;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface CurrencyApiService {
-    List<CurrencyRate> getRate (List<Currencies> currencies);
+    List<CurrencyRate> getRate (List<Currencies> currencies) throws IOException;
 }
---
+++ src/main/java/parser/dto/CurrencyNBUParser.java
@@ -0,0 +1,52 @@
+package parser.dto;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.jsoup.Jsoup;
+import parser.CurrencyApiService;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CurrencyNBUParser implements CurrencyApiService {
+    @Override
+    public List<CurrencyRate> getRate(List<Currencies> currencies) throws IOException {
+        String url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+        String json = Jsoup.connect(url)
+                .ignoreContentType(true)
+                .get()
+                .body()
+                .text();
+        Type typeToken = TypeToken.getParameterized(List.class, CurrencyDtoNBU.class)
+                .getType();
+
+        Gson gson = new Gson();
+        List<CurrencyDtoNBU> currencyDtos = gson.fromJson(json, typeToken);
+        List<CurrencyRate> list = new ArrayList<>();
+        for (int i = 0; i < currencies.size(); i++) {
+            if (currencies.get(i).equals(Currencies.USD)) {
+                CurrencyRate currencyRate = new CurrencyRate();
+                currencyRate.currency = Currencies.USD;
+                currencyRate.sell = null;
+                currencyRate.buy = currencyDtos.stream().filter(it -> it.getCc() == Currencies.USD)
+                        .map(it -> it.getRate()).findFirst().orElseThrow();
+                list.add(currencyRate);
+            } else if (currencies.get(i).equals(Currencies.EUR)) {
+                CurrencyRate currencyRate = new CurrencyRate();
+                currencyRate.currency = Currencies.EUR;
+                currencyRate.sell = null;
+                currencyRate.buy = currencyDtos.stream().filter(it -> it.getCc() == Currencies.EUR)
+                        .map(it -> it.getRate()).findFirst().orElseThrow();
+                list.add(currencyRate);
+            }
+        }
+        return list;
+    }
+
+
+}
+

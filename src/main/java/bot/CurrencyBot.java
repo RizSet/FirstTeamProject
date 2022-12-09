beef92505455d2d6166a38092905186fad4be43a
Volodymyr Kostiuk <kostyukvova2004@gmail.com>
2022-12-09 14:25:07 +0200

    CurrencyButton

--- src/main/java/bot/CurrencyBot.java
+++ src/main/java/bot/CurrencyBot.java
@@ -2,16 +2,19 @@ package bot;
 
 
 import bot.command.StartCommand;
+import buttons.CurrencyButton;
 import fsm.Option;
+import lombok.SneakyThrows;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.HashMap;
 
 public class CurrencyBot extends TelegramLongPollingCommandBot {
     private static HashMap<String, Option> clients = new HashMap<>();
 
-    public HashMap<String, Option> getClients() {
+    public static HashMap<String, Option> getClients() {
         return clients;
     }
 
@@ -29,25 +32,29 @@ public class CurrencyBot extends TelegramLongPollingCommandBot {
         Option optionCurrentChat;
         if (!clients.containsKey(chatId)) {
             clients.put(chatId, new Option());
-        } else {
-            optionCurrentChat = clients.get(chatId);
         }
-        switch (update.getCallbackQuery().getData()) {
-            case ("Отримати інформацію по курсу валют"):
-                break;
-            case ("Налаштування"):
-                break;
-            case ("Кількість знаків після коми"):
-                break;
-            case ("Валюта"):
-                break;
-            case ("Банк"):
+        optionCurrentChat = clients.get(chatId);
+        try {
+            switch (update.getCallbackQuery().getData()) {
+                case ("Отримати інформацію по курсу валют"):
+                    break;
+                case ("Налаштування"):
+                    break;
+                case ("Кількість знаків після коми"):
+                    break;
+                case ("Валюта"):
+                    execute(CurrencyButton.getMessage(chatId));
+                    break;
+                case ("Банк"):
 //                BunkButton.getMessage();
-                break;
-            case ("Час отримання повідомлень"):
-                break;
-            case ("До головного меню"):
-                break;
+                    break;
+                case ("Час отримання повідомлень"):
+                    break;
+                case ("До головного меню"):
+                    break;
+            }
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
         }
     }
 

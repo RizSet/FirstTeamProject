95b3318c8db949205937a389e2eb3e0a1ca46c61
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-12-09 17:03:36 +0200

    TimeMessageButton v.4

--- src/main/java/bot/CurrencyBot.java
+++ src/main/java/bot/CurrencyBot.java
@@ -1,6 +1,6 @@
 package bot;
 
-import bot.buttons.AmountOfSingsAfterCommaButton;
+
 import bot.buttons.GetInfoBotton;
 import bot.buttons.PropertiesButton;
 import bot.buttons.TimeMessageButton;
@@ -16,6 +16,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class CurrencyBot extends TelegramLongPollingCommandBot {
@@ -28,7 +29,7 @@ public class CurrencyBot extends TelegramLongPollingCommandBot {
         return clients;
     }
 
-    public static void setClients(HashMap<String, Option> clients) {
+    public void setClients(HashMap<String, Option> clients) {
         CurrencyBot.clients = clients;
     }
 
@@ -58,7 +59,6 @@ public class CurrencyBot extends TelegramLongPollingCommandBot {
                     execute(PropertiesButton.getMessage(chatId));
                     break;
                 case ("Кількість знаків після коми"):
-                    execute(AmountOfSingsAfterCommaButton.getMessage(chatId));
                     break;
                 case ("Валюта"):
                     break;
@@ -70,21 +70,53 @@ public class CurrencyBot extends TelegramLongPollingCommandBot {
                     break;
                 case ("До головного меню"):
                     break;
-                case ("2"):
-                    execute(AmountOfSingsAfterCommaButton.TwoButton.setSingsAfterComma(update));
-                    break;
-                case ("3"):
-                    execute(AmountOfSingsAfterCommaButton.ThreeButton.setSingsAfterComma(update));
-                    break;
-                case ("4"):
-                    execute(AmountOfSingsAfterCommaButton.FourButton.setSingsAfterComma(update));
-                    break;
             }
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+
+        for (Update item : updates) {
+            String chatId = String.valueOf(item.getMessage().getChatId());
+            Option optionCurrentChat = new Option();
+            if (!clients.containsKey(chatId)) {
+                clients.put(chatId, new Option());
+            } else {
+                optionCurrentChat = clients.get(chatId);
+            }
+            try {
+                if (item.hasMessage()) {
+                    if (item.getMessage().getText().equals("На головне меню")) {
+                        //menu buttoon
+                        break;
+                    }
+                    execute(ChooseTime.setTimeNotation(chatId, item.getMessage().getText(), timer));
+                    if(!timer.isNotationOn()) {
+                        Option finalOptionCurrentChat = optionCurrentChat;
+                        timer.scheduledExecutorService.scheduleAtFixedRate(
+                                () -> {
+                                    try {
+                                        execute(GetInfoBotton.getInfoMessage(chatId, finalOptionCurrentChat));
+                                    } catch (TelegramApiException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                },
+                                5,
+                                5,
+                                TimeUnit.SECONDS);
+                    } else {
+                        timer.scheduledExecutorService.shutdown();
+                    }
+                }
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
 
     @Override
     public String getBotUsername() {

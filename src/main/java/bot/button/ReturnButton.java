b7b366789776082d47ec985c786df5c466619a1b
Dmytro Kononenko <66216836+rizset@users.noreply.github.com>
2022-12-11 15:10:25 +0200

    Merge pull request #9 from RizSet/AmountOfSingsAfterCommaButton
    Amount of sings after comma button

--- src/main/java/bot/button/ReturnButton.java
+++ src/main/java/bot/button/ReturnButton.java
@@ -1,4 +1,4 @@
-package fsm;
+package bot.button;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;

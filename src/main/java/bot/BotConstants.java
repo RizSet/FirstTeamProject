933767ae718599817d2e4d68b34679527a3ab155
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-12-12 21:49:45 +0200

    fix bags

--- src/main/java/bot/BotConstants.java
+++ src/main/java/bot/BotConstants.java
@@ -2,5 +2,5 @@ package bot;
 
 public class BotConstants {
     public static final String BOT_NAME = "Currency in Schedule";
-    public static final String BOT_TOKEN = "5724700638:AAEj3380IQoZXHaTPA6CLhVRdKgPkO-5_8U";
+    public static final String BOT_TOKEN = "5936285999:AAF59ijICUjLPqtoxqLCndmXnQIGE-jzGbQ";
 }
